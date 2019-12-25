2fd08f1847bfda072b05093055453ccc0f61a260
manleebk <levanman1997@gmail.com>
2019-12-25 08:05:38 +0700

    Fix bug: comment
    Fix bug: comment

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -18,7 +18,7 @@ class AdminMiddleware
     {
         if (Auth::check()) {
             $user=Auth::user();
-            if ($user->level==1) 
+            if ($user->level==1 || $user->level == 0) 
                 return $next($request);
             else
                 return redirect('/dangnhap');
