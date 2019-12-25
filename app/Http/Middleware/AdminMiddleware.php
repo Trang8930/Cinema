956bd01dc82d1f858a3ca058e8a0076de158139b
manleebk <levanman1997@gmail.com>
2019-12-25 09:26:33 +0700

    Revert "Fix bug: comment"
    This reverts commit 2fd08f1847bfda072b05093055453ccc0f61a260.

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -18,7 +18,7 @@ class AdminMiddleware
     {
         if (Auth::check()) {
             $user=Auth::user();
-            if ($user->level==1 || $user->level == 0) 
+            if ($user->level==1) 
                 return $next($request);
             else
                 return redirect('/dangnhap');
